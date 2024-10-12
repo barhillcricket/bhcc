69437be03e7ffb43de97b8937424b9e4903c5ebb
tompage1994@hotmail.co.uk <tpage@redhat.com>
2024-10-12 17:44:55 +0100

    add wicket taker

---
+++ dev/updatestats/updateWicketTakers.py
@@ -0,0 +1,34 @@
+# This assumes that the AllTimeBowling has already been updated
+
+import csv
+import sys
+
+alltime_stats = []
+pc_stats = []
+
+with open('newstats/AllTimeBowling.csv', newline='') as existing:
+    reader = csv.reader(existing, delimiter=',')
+    for row in reader:
+        alltime_stats.append(row)
+alltime_stats.pop(0)
+
+topBowlers = []
+
+for p in alltime_stats:
+    if int(p[6]) >= 100:
+        topBowlers += [[p[0], p[1], p[2], p[3], p[4], p[6], p[5], p[7], p[11]]]
+
+
+# Sort by wickets
+def sortByWickets(player):
+    return -int(player[5])
+
+
+topBowlers.sort(key=sortByWickets)
+
+# Write back to newstats/AllTimeWicket.csv
+with open('newstats/AllTimeWicket.csv', 'w', newline='') as alltime:
+    writer = csv.writer(alltime, delimiter=',')
+    writer.writerow(["Rank", "Player", "Matches", "Overs", "Mdn", "Wkt", "Runs", "Best", "Ave"])
+    for row in topBowlers:
+        writer.writerow(row)
